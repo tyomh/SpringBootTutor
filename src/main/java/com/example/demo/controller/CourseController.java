5bf80dbda1e01fe42b826a96d2388e11f558fff5
minghe <minghez2@uci.edu>
2018-11-04 15:28:52 -0800

    modification

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import com.example.demo.modal.Course;
+import com.example.demo.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +17,14 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class CourseController {
+    @Autowired
+    CourseService courseService;
 
     @GetMapping(path = "/", produces = "application/json")
     public HttpEntity findAllCourses(){
-       // List<CourseDto> allCourses = courseService.findAllCourses();
+       List<Course> allCourses = courseService.findAllCourses();
 
-        return new ResponseEntity<>("OK",HttpStatus.OK);
+        return new ResponseEntity<>(allCourses,HttpStatus.OK);
     }
 
 //    @GetMapping(path = "/api/course/findAllCourses", produces = "application/json")
